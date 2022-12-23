4dd69a8cf1d495ddc032c8bd65595177b6a9474e
eliasfernandescout <eliasfernandescout@gmail.com>
2022-12-22 22:35:33 -0300

    update

--- test/Order.test.ts
+++
@@ -1,8 +0,0 @@
-import Order from "../src/Order";
-
-
-test("Nao deve criar um pedido com o CPF invalido", function () {
-    expect(() => new Order("111.111.111-11")).toThrow(new Error("CPF invalido"));
-     
-
-})
