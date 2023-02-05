3da4aad15306244778d95cdfbd7c00b95fab2a24
eliasfernandescout <eliasfernandescout@gmail.com>
2023-02-04 23:33:54 -0300

    update

--- src/OrderController.ts
+++ src/OrderController.ts
@@ -1,9 +1,13 @@
 import express from "express";
-const app = express;
+import Preview from "./Preview";
+const app = express();
 
 
 
-app.post("/preview" function(request, response){
+app.post("/preview", function(request, response){
+
+    const preview = new Preview();
+    preview.execute(input)
 
 })
 app.listen(3000);
