9f9558ef546f692614896734b1d1179e6ec509a4
eliasfernandescout <eliasfernandescout@gmail.com>
2023-02-06 09:05:39 -0300

    add preview

--- src/Preview.ts
+++ src/Preview.ts
@@ -1,10 +1,30 @@
+import ItemRepository from "./ItemRepository"
+import Order from "./Order"
+
 export default class Preview {
-    constructor(){
+    constructor(readonly itemRepository: ItemRepository){
 
     }
 
-    execute(){
-        
+    async execute(input: Input): Promise<Output>{
+        const order = new Order(input.cpf)
+        for(const orderItem of input.orderItems){
+            const item = await this.itemRepository.getItem(orderItem.idItem);
+            order.addItem(item, orderItem.quantity)
+
+        }
+        const total = order.getTotal();
+        return {total};
+
 
     }
+}
+
+type Input = {
+    cpf: string,
+    orderItems: {idItem: number, quantity: number}[]
+}
+
+type Output = {
+    total: number
 }
