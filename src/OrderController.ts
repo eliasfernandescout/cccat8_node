848d7982c7b14e26a4d5ef1aa19eb461c1e452ef
eliasfernandescout <eliasfernandescout@gmail.com>
2023-02-07 08:53:12 -0300

    update

--- src/OrderController.ts
+++ src/OrderController.ts
@@ -1,21 +1,38 @@
 import express from "express";
 import Checkout from "./application/Checkout";
+import GetOrderByCpf from "./application/GetOrdersByCpf";
 import Preview from "./application/Preview";
+import Item from "./domain/Item";
+import ItemRepositoryMemory from "./ItemRepositoryMemory";
+import OrderRepositoryMemory from "./OrderRepositoryMemory";
 const app = express();
 
+const itemRepository = new ItemRepositoryMemory();
+itemRepository.save(new Item(1, "Guitarra", 1000));
+itemRepository.save(new Item(1, "Amplificador", 5000));
+itemRepository.save(new Item(1, "Cabo", 30));
+
+const orderRepository = new OrderRepositoryMemory();
 
 
-app.post("/preview", function(request, response){
-    const preview = new Preview();
-    const total = preview.execute(input)
+app.post("/preview", async function(request, response){
+    const preview = new Preview(itemRepository);
+    const total = await preview.execute(request.body);
     response.json({total});
 
 });
 
-app.post("/checkout", function(request, response){
-    const preview = new Checkout();
-    checkout.execute(input)
+app.post("/checkout", async function(request, response){
+    const checkout = new Checkout(itemRepository, orderRepository);
+    await checkout.execute(request.body);
     response.end()
+
+});
+
+app.get("/orders/:cpf", async function(request, response){
+    const getOrderByCpf = new GetOrderByCpf(orderRepository);
+    const orders = await getOrderByCpf.execute(request.params.cpf);
+    response.json(orders);
 
 });
 
