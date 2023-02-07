77640a6060f62b38f08aae745b77ec2ae6630b4c
eliasfernandescout <eliasfernandescout@gmail.com>
2023-02-07 08:37:34 -0300

    update

---
+++ src/OrderRepositoryMemory.ts
@@ -0,0 +1,19 @@
+import Order from "./domain/Order";
+import OrderRepository from "./OrderRepository";
+
+export default class OrderRepositoryMemory implements OrderRepository {
+    orders: Order[];
+
+    constructor(){
+        this.orders = [];
+    }
+    
+    async getByCpf(cpf: string): Promise<Order[]> {
+        return this.orders.filter(order => order.cpf.value === cpf)
+    }
+    async save(order: Order): Promise<void>{
+        this.orders.push(order);
+
+    }
+
+}
